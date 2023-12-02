af9d62144268e66573a4cefd206077f166a34963
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-02 17:07:34 +0800

    Create 1.鸢鸟花数据集iris.py

---
+++ 1.鸢鸟花数据集iris.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+#将matplotlib库中的pyplot模块导入，并为其指定一个简短的别名plt 
+import numpy as np  
+#将numpy库导入，并为其指定一个简短的别名np
+from sklearn.cluster import KMeans
+from sklearn import datasets    #为了导入 iris 鸢鸟花数据集
+ 
+iris = datasets.load_iris() 
+# SepalLength PetalLength SepalWidth PetalWidth Class
+#5.1 3.5  1.4 0.2 Iris-setosa    4+1行
+#150列
+
