8d38e8522019964a3363c120008c6bbc0156bc53
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-02 17:35:18 +0800

    Update 4.print(X.shape).py

--- 4.print(X.shape).py
+++ 4.print(X.shape).py
@@ -1,5 +1,3 @@
-import matplotlib.pyplot as plt 
-#将matplotlib库中的pyplot模块导入，并为其指定一个简短的别名plt 
 import numpy as np  
 #将numpy库导入，并为其指定一个简短的别名np
 from sklearn.cluster import KMeans
@@ -14,7 +12,12 @@ X = iris.data[:, :4]  # #表示我们取特征空间中的4个维度
 #切片 和X2 一样 先行在列
 
 print(X.shape)   # (150, 4)
-# 和X2 一样 先行在列
+# 和X2 一样 先行在列 例如 (150, 4) 表示X有150行和4列。
+
+
+
+
+
 
 
 
