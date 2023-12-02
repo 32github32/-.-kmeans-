6974859f9b023ee62c4fa6fc168cb66643e01db2
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-02 23:58:44 +0800

    Update 7.逻辑回归Logistic_路径.py

--- 7.逻辑回归Logistic_路径.py
+++ 7.逻辑回归Logistic_路径.py
@@ -1,1 +1,38 @@
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv(r"E:\桌面\课件 (2)\老师给的代码\BreastCancerData.csv")
+target = 'Class'
+ID = 'Code number'
+
+#1.1 生成数据
+y = data[target]
+data_columns = [x for x in data.columns if x not in [target, ID] ]
+x = data[data_columns]
+
+# 1.2 划分训练集和测试集 XXYY 
+xTrain, xTest, yTrain, yTest = train_test_split(x, y) 
+# 2 建模
+NewModel = LogisticRegression()
+# 3 训练
+NewModel = NewModel.fit(xTrain,yTrain)
+# 4 预测
+yTrainPred = NewModel.predict(xTrain)
+yTestPred = NewModel.predict(xTest)
+
+print(confusion_matrix(yTrain, yTrainPred))
+"""
+[[334   7]
+ [  8 175]]
+"""
+print(confusion_matrix(yTest, yTestPred))
+"""
+[[114   3]
+ [  3  55]]
+"""
+
