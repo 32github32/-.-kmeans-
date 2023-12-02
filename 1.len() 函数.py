1666b98ac9404cc92977824853bb63f1bf6c2043
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-03 00:13:00 +0800

    Create 1.len() 函数.py

---
+++ 1.len() 函数.py
@@ -0,0 +1,14 @@
+# 获取字符串的长度
+s = "Hello, World!"
+print(len(s))  # 输出 13
+
+# 获取列表的长度
+my_list = [1, 2, 3, 4, 5]
+print(len(my_list))  # 输出 5
+
+# 获取字典中键值对的个数
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print(len(my_dict))  # 输出 3
+
+
+
