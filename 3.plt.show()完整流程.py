28fd37858417c86f317248d3a3ee24278991f919
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-02 19:23:21 +0800

    3.plt.show()完整流程.py

--- 3.plt.show()完整流程.py
+++ 3.plt.show()完整流程.py
@@ -17,3 +17,26 @@ plt.show()
 
 
 #最简单的matplotlib.pyplot 使用方法： 数据 + 线图（这步已经有图了） +  标签题目  +  显示
+——————————————————————————————————————————————————————————————————————————————————————————
+
+# 5 绘图
+plt.figure()  #三图合一
+
+#第一张图 x y c
+d0 = irisdata[clustering.labels_ == 0]  
+plt.plot(d0[:, 0], d0[:, 1], 'r.')
+
+#第二张图 x y c
+d1 = irisdata[clustering.labels_ == 1]
+plt.plot(d1[:, 0], d1[:, 1], 'go')
+
+#第三张图 x y c
+d2 = irisdata[clustering.labels_ == 2]
+plt.plot(d2[:, 0], d2[:, 1], 'b*')
+
+plt.xlabel("Sepal.Length")
+plt.ylabel("Sepal.Width")
+plt.title("AGNES Clustering")
+
+plt.show()
+——————————————————————————————————————————————————————————————————————————————————————————
