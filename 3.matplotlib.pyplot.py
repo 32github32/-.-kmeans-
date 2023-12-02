d24b97278a2dc7bf89fbb5d5740134802f71d496
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-02 14:27:49 +0800

    Create 3.matplotlib.pyplot.py

---
+++ 3.matplotlib.pyplot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+# 创建数据
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# 绘制线图
+plt.plot(x, y)
+
+# 添加标题和标签
+plt.title("My First Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# 显示图表
+plt.show()
+
+
+#最简单的matplotlib.pyplot 使用方法： 数据 + 线图（这步已经有图了） +  标签题目  +  显示
