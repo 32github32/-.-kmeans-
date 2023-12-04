daecb7d5ca1884800a31d718c5b7ca7bfcfb0a5a
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 14:30:03 +0800

    Create 2.list.append()_加入.py

---
+++ 2.list.append()_加入.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)  # 输出: [1, 2, 3, 4]
+
+
+my_list = []
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+print(my_list)  # 输出: [1, 2, 3]
