e2620bc09cbc630cd31d9dfbc04690cb7e2ecbfe
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 16:06:06 +0800

    Create 2.dict.keys()_键列表.py

---
+++ 2.dict.keys()_键列表.py
@@ -0,0 +1,16 @@
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+
+# 使用 keys() 方法获取字典的键
+keys = my_dict.keys()
+
+print("字典的键:", keys)
+#字典的键: dict_keys(['name', 'age', 'city'])
+
+#————————————————————————————————————————————————————————————
+
+key_list = list(my_dict.keys())
+
+print("转换为列表:", key_list)
+
+
+#转换为列表: ['name', 'age', 'city']
