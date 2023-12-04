f0f7054135e2fd9cf0c4c77219ea282fb81a283b
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 15:52:35 +0800

    Create 2.dict.clear()_清空.py

---
+++ 2.dict.clear()_清空.py
@@ -0,0 +1,11 @@
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+
+print("原始字典:", my_dict)
+
+my_dict.clear()
+
+print("清空后的字典:", my_dict)
+
+
+#原始字典: {'name': 'Alice', 'age': 25, 'city': 'New York'}
+#清空后的字典: {}
