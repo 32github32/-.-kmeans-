863874af3f22b4f1dfd58098e232e5eaf0588d1e
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 15:54:20 +0800

    Create 2.dict.copy()_复制.py

---
+++ 2.dict.copy()_复制.py
@@ -0,0 +1,11 @@
+my_dict = {"name": "Alice", "age": 25, "city": "New York"}
+
+# 使用 copy() 方法创建字典副本
+new_dict = my_dict.copy()
+
+print("原始字典:", my_dict)
+print("复制的字典:", new_dict)
+
+
+#原始字典: {'name': 'Alice', 'age': 25, 'city': 'New York'}
+#复制的字典: {'name': 'Alice', 'age': 25, 'city': 'New York'}
