094d65580ed57d79c66b5106c31cf597c131f0b3
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 16:08:20 +0800

    Create 2.dict.values()_值列表.py

---
+++ 2.dict.values()_值列表.py
@@ -0,0 +1,18 @@
+# 创建一个字典
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# 使用values()方法获取值视图对象，并进行迭代
+for value in my_dict.values():
+    print(value)
+
+
+#1
+#2
+#3
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+values_list = list(my_dict.values())
+print(values_list)
+
+
+#[1, 2, 3]
