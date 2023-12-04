8f7d3d8c748eccafb6034e0bd8110753a43dca5c
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 15:14:45 +0800

    Create 1.min()_找最小值.py

---
+++ 1.min()_找最小值.py
@@ -0,0 +1,20 @@
+my_list = [3, 1, 4, 1, 5, 9, 2, 6]
+minimum = min(my_list)
+print(minimum)  # 输出: 1
+
+
+my_tuple = (7, 2, 8, 5, 3)
+minimum = min(my_tuple)
+print(minimum)  # 输出: 2
+
+my_set = {10, 20, 15, 30}
+minimum = min(my_set)
+print(minimum)  # 输出: 10
+
+
+
+a = 10
+b = 5
+c = 7
+minimum = min(a, b, c)
+print(minimum)  # 输出: 5
