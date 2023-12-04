045d3018f53e745a429c3c896c4cba34c32cb2e2
Wang SH <151306722+32github32@users.noreply.github.com>
2023-12-04 15:17:23 +0800

    Update and rename 1.len() 函数.py to 1.len()_算总数.py

---
+++ 1.len()_算总数.py
@@ -0,0 +1,36 @@
+# 获取字符串的长度
+s = "Hello, World!"
+print(len(s))  # 输出 13
+
+# 获取列表的长度
+my_list = [1, 2, 3, 4, 5]
+print(len(my_list))  # 输出 5
+
+# 获取字典中键值对的个数
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+print(len(my_dict))  # 输出 3
+
+
+my_list = [1, 2, 3, 4, 5]
+length = len(my_list)
+print(length)  # 输出: 5
+
+
+my_string = "Hello, World!"
+length = len(my_string)
+print(length)  # 输出: 13
+
+my_tuple = (7, 8, 9)
+length = len(my_tuple)
+print(length)  # 输出: 3
+
+my_set = {10, 20, 30, 40}
+length = len(my_set)
+print(length)  # 输出: 4
+
+
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+length = len(my_dict)
+print(length)  # 输出: 3
+
